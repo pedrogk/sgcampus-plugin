633511a396ad05db36922985edf4d8d17a901c87
pedrogk <pedro@sg.com.mx>
2014-02-03 02:02:24 -0600

    Added webinars to query

--- sgcampus.php
+++ sgcampus.php
@@ -27,6 +27,12 @@ function create_post_type() {
 	register_taxonomy_for_object_type( 'post_tag', 'webinar' );
 }
 
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
+function add_my_post_types_to_query( $query ) {
+	if ( is_home() && $query->is_main_query() )
+		$query->set( 'post_type', array( 'post', 'page', 'webinar' ) );
+	return $query;
+}
 
 ?>
