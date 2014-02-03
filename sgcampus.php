058c8abdc5713d4a69e6c537946052862935f0fb
pedrogk <pedro@sg.com.mx>
2014-02-02 21:46:22 -0600

    Added sgcampus.php

---
+++ sgcampus.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: SG Campus stuff
+   Plugin URI: http://sgcampus.com.mx
+   Description: Plugin for SG Campus specific stuff
+   Version: 0.9
+   Author: Pedro Galvan
+   Author URI: http://sg.com.mx
+   License: MIT
+   */
+?>
