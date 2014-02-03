bf897558760e40de28777cdf426db90216847bde
pedrogk <pedro@sg.com.mx>
2014-02-03 04:09:51 -0600

    Added webinars to query

--- sgcampus.php
+++ sgcampus.php
@@ -28,7 +28,7 @@ function create_post_type() {
 }
 
 add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
-
+/*
 function add_my_post_types_to_query( $query ) {
 	if ( is_home() && $query->is_main_query() )
 			$query->set( 'post_type', array( 'post', 'webinar' ) );
@@ -36,5 +36,5 @@ function add_my_post_types_to_query( $query ) {
 			$query->set( 'post_type', array( 'post', 'webinar' ) );
 	return $query;
 }
-
+*/
 ?>
