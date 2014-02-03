b756248681757f57c20429e8134015d6dd2a3dab
pedrogk <pedro@sg.com.mx>
2014-02-02 23:54:02 -0600

    Adding code to save meta

--- sgcampus.php
+++ sgcampus.php
@@ -3,7 +3,7 @@
    Plugin Name: SG Campus stuff
    Plugin URI: http://sgcampus.com.mx
    Description: Plugin for SG Campus specific stuff
-   Version: 0.9
+   Version: 0.10
    Author: Pedro Galvan
    Author URI: http://sg.com.mx
    License: MIT
